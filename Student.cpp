c0b2efdb23955cad8dad7185378f176b061b860c
Mohamed Tarek <mohkka7@gmail.com>
2025-04-24 21:24:18 +0200

    DS Project

---
+++ Student.cpp
@@ -0,0 +1,237 @@
+#include "Student.h"
+#include <iomanip>
+#include <iostream>
+#include <algorithm>
+#include <limits>
+#include <stdexcept>
+#include <utility>
+
+Student::Student(std::string  name, std::string  id,
+                 std::string  year, std::string  email)
+    : name(std::move(name)), id(std::move(id)), year(std::move(year)), email(std::move(email)) {}
+
+void Student::addCompletedCourse(const CompletedCourse& course) {
+    if (convertGradeToGPA(course.grade) == -1) {
+        throw std::invalid_argument("Invalid grade provided for completed course");
+    }
+    completedCourses.push_back(course);
+}
+
+void Student::addAvailableCourse(const Course& course) {
+    availableCourses.push_back(course);
+}
+
+void Student::viewGrade() const {
+    if (completedCourses.empty()) {
+        std::cout << "No Courses Completed.\n";
+        return;
+    }
+
+    size_t maxCourseNameWidth = 12;
+    size_t maxSemesterWidth = 10;
+    size_t maxGradeWidth = 7;
+
+    for (const auto &completedCourse : completedCourses) {
+        maxCourseNameWidth = std::max(maxCourseNameWidth, completedCourse.course.getTitle().length());
+        maxSemesterWidth = std::max(maxSemesterWidth, completedCourse.semester.length());
+        maxGradeWidth = std::max(maxGradeWidth, completedCourse.grade.length());
+    }
+
+    std::cout << std::left << std::setw(maxCourseNameWidth) << "Course Name" << " | "
+              << std::left << std::setw(maxSemesterWidth) << "Semester" << " | "
+              << std::left << std::setw(maxGradeWidth) << "Grade" << "\n";
+
+    std::cout << std::string(maxCourseNameWidth + maxSemesterWidth + maxGradeWidth + 6, '-') << "\n";
+
+    for (const auto &completedCourse : completedCourses) {
+        std::cout << std::left << std::setw(maxCourseNameWidth) << completedCourse.course.getTitle() << " | "
+                  << std::left << std::setw(maxSemesterWidth) << completedCourse.semester << " | "
+                  << std::left << std::setw(maxGradeWidth) << completedCourse.grade << "\n";
+    }
+}
+
+double Student::convertGradeToGPA(const std::string &grade) {
+    if (grade == "A+" || grade == "A") return 4.0;
+    if (grade == "A-") return 3.8;
+    if (grade == "B+") return 3.7;
+    if (grade == "B") return 3.4;
+    if (grade == "B-") return 3.1;
+    if (grade == "C+") return 2.8;
+    if (grade == "C") return 2.5;
+    if (grade == "C-") return 2.2;
+    if (grade == "D+") return 1.8;
+    if (grade == "D") return 1.5;
+    if (grade == "D-") return 1.2;
+    if (grade == "F") return 0.0;
+    return -1; // Invalid grade
+}
+
+double Student::calcGPA() const {
+    double totalGradePoints = 0;
+    double totalCreditHours = 0;
+
+    for (const auto &completedCourse : completedCourses) {
+        double gpaGrade = convertGradeToGPA(completedCourse.grade);
+        if (gpaGrade != -1) {
+            double creditHour = completedCourse.course.getCreditHour();
+            totalGradePoints += gpaGrade * creditHour;
+            totalCreditHours += creditHour;
+        }
+    }
+
+    if (totalCreditHours == 0) {
+        return 0.0;
+    }
+
+    return totalGradePoints / totalCreditHours;
+}
+
+void Student::showAvailableCourses() const {
+    if (availableCourses.empty()) {
+        std::cout << "No available courses to show.\n";
+        return;
+    }
+
+    for (const auto &course : availableCourses) {
+        std::cout << "\n";
+        course.showCourseDescription();
+    }
+}
+
+bool Student::checkPrerequisite(const Course &course) const {
+    std::vector<Course> prerequisites = course.getPrerequisites();
+
+    for (const Course &prereq : prerequisites) {
+        bool found = false;
+
+        for (const auto &completed : completedCourses) {
+            if (completed.course.getCourseID() == prereq.getCourseID()) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void Student::leftPrerequisites(std::deque<Course> leftCourses) const {
+    if (leftCourses.empty()) {
+        std::cout << "You have no remaining prerequisites.\n";
+        return;
+    }
+
+    std::cout << "❌ You still need to complete the following prerequisites:\n";
+    while (!leftCourses.empty()) {
+        Course c = leftCourses.front();
+        leftCourses.pop_front();
+        std::cout << " - " << c.getTitle() << " (ID: " << c.getCourseID() << ")\n";
+    }
+}
+
+void Student::registerCourse() {
+    if (availableCourses.empty()) {
+        std::cout << "No courses available for registration.\n";
+        return;
+    }
+
+    showAvailableCourses();
+
+    std::string courseID;
+    std::cout << "Please enter the course ID: ";
+    std::cin >> courseID;
+
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    bool found = false;
+
+    for (const auto &course : availableCourses) {
+        if (course.getCourseID() == courseID) {
+            found = true;
+
+            if (checkPrerequisite(course)) {
+                registeredCourses.push_back(course);
+                std::cout << "✅ Course \"" << course.getTitle() << "\" registered successfully!\n";
+            } else {
+                std::cout << "❌ You do not meet the prerequisites for \"" << course.getTitle() << "\".\n";
+
+                std::deque<Course> leftCourses;
+                for (const Course &prereq : course.getPrerequisites()) {
+                    bool foundPrereq = false;
+
+                    for (const auto &completed : completedCourses) {
+                        if (completed.course.getCourseID() == prereq.getCourseID()) {
+                            foundPrereq = true;
+                            break;
+                        }
+                    }
+
+                    if (!foundPrereq) {
+                        leftCourses.push_back(prereq);
+                    }
+                }
+
+                leftPrerequisites(leftCourses);
+            }
+            break;
+        }
+    }
+
+    if (!found) {
+        std::cout << "❌ Course ID not found in available courses.\n";
+    }
+}
+
+void Student::dropCourse(const std::string &courseID) {
+    auto it = std::find_if(registeredCourses.begin(), registeredCourses.end(), [&](const Course& course) {
+        return course.getCourseID() == courseID;
+    });
+
+    if (it != registeredCourses.end()) {
+        std::cout << " Dropped course: " << it->getTitle() << " (ID: " << it->getCourseID() << ")\n";
+        registeredCourses.erase(it);
+    } else {
+        std::cout << "⚠️ You are not registered in a course with ID: " << courseID << "\n";
+    }
+}
+
+void Student::generateTranscript() const {
+    std::cout << "\n=========== STUDENT TRANSCRIPT ===========\n";
+    std::cout << " Name  : " << name << "\n";
+    std::cout << " ID    : " << id << "\n";
+    std::cout << " Email : " << email << "\n";
+    std::cout << " Year  : " << year << "\n";
+    std::cout << " GPA   : " << std::fixed << std::setprecision(2) << calcGPA() << "\n";
+
+    std::cout << "\n Completed Courses:\n";
+    if (completedCourses.empty()) {
+        std::cout << " --- No courses completed yet---\n";
+    } else {
+        std::cout << std::left << std::setw(25) << "Course Name"
+                  << std::setw(15) << "Semester"
+                  << std::setw(10) << "Grade" << "\n";
+
+        std::cout << std::string(50, '-') << "\n";
+        for (const auto& course : completedCourses) {
+            std::cout << std::left << std::setw(25) << course.course.getTitle()
+                      << std::setw(15) << course.semester
+                      << std::setw(10) << course.grade << "\n";
+        }
+    }
+
+    std::cout << "\n Registered Courses:\n";
+    if (registeredCourses.empty()) {
+        std::cout << " - No courses currently registered.\n";
+    } else {
+        for (const auto& course : registeredCourses) {
+            std::cout << " - " << course.getTitle() << " (ID: " << course.getCourseID()
+                      << ", Credit Hours: " << course.getCreditHour() << ")\n";
+        }
+    }
+
+    std::cout << "===========================================\n";
+}
---
+++ System_Manager.cpp
@@ -0,0 +1,353 @@
+//
+// Created by msi on 20/04/2025.
+//
+
+#include "System_Manager.h"
+
+void System_Manager::editAdminPass(string username, string password) {
+
+    int i = 3;
+    while (i--) {
+        if (admins.find(username) == admins.end()) {
+            cout << "This username does not exist." << endl;
+            return;
+        } else {
+            string oldPassword;
+            cout << "Enter your old password:" << endl;
+            cin >> oldPassword;
+            if (admins[username] == oldPassword) {
+                admins[username] = password;
+                cout << "Password successfully changed." << endl;
+                return;
+            } else {
+                cout << "Password is incorrect." << endl;
+            }
+        }
+    }
+
+    cout << "Too many tries, try again later." << endl;
+}
+
+
+void System_Manager::editStudentPass(string id, string password) {
+    int i = 3;
+    while (i--) {
+        if (students.find(id) == students.end()) {
+            cout << "This ID does not exist." << endl;
+            return;
+        } else {
+            string oldPassword;
+            cout << "Enter your old password:" << endl;
+            cin >> oldPassword;
+            if (students[id] == oldPassword;) {
+                students[id] = password;
+                cout << "Password successfully changed." << endl;
+                return;
+            } else {
+                cout << "Password is incorrect." << endl;
+            }
+        }
+    }
+
+    cout << "Too many tries, try again later." << endl;
+}
+
+Student System_Manager::getStudent(string id) {
+    return students[id];
+}
+
+Admin System_Manager::getAdmin(string username) {
+    return admins[username];
+}
+
+Course System_Manager::getCourse(string courseID) {
+    return courses[courseID];
+}
+
+void System_Manager::showAvailableCourses() {
+    cout << "Available courses:" << endl;
+    for(auto course : courses) {
+        cout << "Course ID: " << course.getCourseID() << endl;
+        cout << "Title: " << course.getTitle() << endl;
+        cout << "Credit Hours: " << course.getCreditHour() << endl;
+        cout << "Instructor: " << course.getInstructorName() << endl;
+        cout << "Syllabus: " << course.getSyllabus() << endl;
+
+    }
+}
+
+void System_Manager::showEligibleCourses(string id) {
+    if (students.find(id) == students.end()) {
+        cout << "Invalid entry." << endl;
+    } else {
+        Student student = students[id];
+        stack<Course> eligibleCourses;
+
+        for (const auto& [courseID, course] : courses) {
+            bool eligible = true;
+
+            for (auto prereq : course.getPrerequisites()) {
+                bool found = false;
+
+                string CourseID = prereq.getCourseID();
+                    if (student.completedCourses.find(CourseID) != student.completedCourses.end()) {
+                        found = true;
+                    }
+
+                if (!found) {
+                    eligible = false;
+                    break;
+                }
+            }
+
+            if (eligible) {
+                eligibleCourses.push(course);
+            }
+        }
+
+        // Display eligible courses
+        if (eligibleCourses.empty()) {
+            std::cout << "No eligible courses found for " << student.getName() << ".\n";
+            return;
+        }
+
+        cout << "Eligible courses for " << student.getName() << ":\n";
+        while (!eligibleCourses.empty()) {
+            eligibleCourses.top().showCourseDescription();
+            eligibleCourses.pop();
+        }
+
+    }
+}
+
+bool isStudentEligible(string id, string courseCode) {
+    if (students.find(id) == students.end() || courses.find(courseCode) == courses.end()) {
+        return false;
+    }
+
+    Student student = students[id];
+    Course course = courses[courseCode];
+
+    for (const Course& prereq : course.getPrerequisites()) {
+        string prereqID = prereq.getCourseID();
+        if (student.completedCourses.find(prereqID) == student.completedCourses.end()) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+//--------------------------------------------- FILES FUNCTIONS---------------------------------------------
+
+//Students Functions
+
+void System_Manager::readStudentsFromFile() {
+    std::ifstream file("students.csv");
+    if (!file.is_open()) {
+        std::cerr << " Failed to open file for reading:students.csv \n " ;
+        return;
+    }
+
+    students.clear();
+    std::string line;
+
+    while (std::getline(file, line)) {
+        std::stringstream ss(line);
+        std::string name, id, year, email, gpaStr, creditHoursStr, completedStr, registeredStr;
+
+        std::getline(ss, name, ',');
+        std::getline(ss, id, ',');
+        std::getline(ss, year, ',');
+        std::getline(ss, email, ',');
+        std::getline(ss, gpaStr, ',');
+        std::getline(ss, creditHoursStr, ',');
+        std::getline(ss, completedStr, ',');
+        std::getline(ss, registeredStr, ',');
+
+        Student student(name, id, year, email);
+
+        // Parse completed courses
+
+        std::stringstream completedStream(completedStr);
+        std::string courseID;
+        while (std::getline(completedStream, courseID, '|')) {
+            Course* c = getCourseByID(courseID);
+            if (c) {
+                CompletedCourse cc(*c, "N/A", "A");
+                student.addCompletedCourse(cc);
+            }
+        }
+
+        // Parse registered courses
+        std::stringstream regStream(registeredStr);
+        while (std::getline(regStream, courseID, '|')) {
+            Course* c = getCourseByID(courseID);
+            if (c) {
+                student.addAvailableCourse(*c);
+                student.registerCourse();
+            }
+        }
+
+        student.setCompletedCreditHours(std::stoi(creditHoursStr));
+        students.push_back(student);
+    }
+
+    file.close();
+}
+
+
+void System_Manager::writeStudentsToFile() {
+    std::ofstream file("students.csv");
+    if (!file.is_open()) {
+        std::cerr << " Failed to open file for writing:students.csv\n ";
+        return;
+    }
+
+    for (const auto& student : students) {
+        file << student.getName() << ","
+             << student.getID() << ","
+             << student.getYear() << ","
+             << student.getEmail() << ","
+             << std::fixed << std::setprecision(2) << student.calcGPA() << ","
+             << student.getCompletedCreditHours() << ",";
+
+        // Completed Courses IDs
+        for (size_t i = 0; i < student.getCompletedCourses().size(); ++i) {
+            file << student.getCompletedCourses()[i].course.getCourseID();
+            if (i != student.getCompletedCourses().size() - 1)
+                file << "|";
+        }
+        file << ",";
+
+        // Registered Courses IDs
+        for (size_t i = 0; i < student.getRegisteredCourses().size(); ++i) {
+            file << student.getRegisteredCourses()[i].getCourseID();
+            if (i != student.getRegisteredCourses().size() - 1)
+                file << "|";
+        }
+
+        file << "\n";
+    }
+
+    file.close();
+}
+
+
+
+//Admins Functions
+
+void System_Manager::readAdminsFromFile() {
+    std::ifstream file("admins.csv");
+    if (!file.is_open()) {
+        std::cerr << " Failed to open file for reading: admins.csv\n";
+        return;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        std::stringstream ss(line);
+        std::string name, id, username, password;
+
+        std::getline(ss, name, ',');
+        std::getline(ss, id, ',');
+        std::getline(ss, username, ',');
+        std::getline(ss, password, ',');
+
+
+        Admin admin(id, name, username, password);
+        admins[username] = admin;
+    }
+
+    file.close();
+}
+
+void System_Manager::writeAdminsToFile() {
+    std::ofstream file("admins.csv");
+    if (!file.is_open()) {
+        std::cerr << " Failed to open file for writing: admins.csv\n";
+        return;
+    }
+
+    for (const auto& entry : admins) {
+        const Admin& admin = entry.second;
+        file << admin.getName() << ","
+             << admin.getID() << ","
+             << admin.getUsername() << ","
+             << admin.getPassword() << "\n";
+    }
+
+    file.close();
+}
+
+
+
+
+
+//Courses Function
+
+void System_Manager::readCoursesFromFile() {
+    std::ifstream file("courses.csv");
+    if (!file.is_open()) {
+        std::cerr << " Failed to open courses.csv for reading\n";
+        return;
+    }
+
+    courses.clear();
+    std::string line;
+    while (std::getline(file, line)) {
+        std::stringstream ss(line);
+        std::string id, title, creditStr, instructorName, instructorEmail, prereqStr;
+
+        std::getline(ss, id, ',');
+        std::getline(ss, title, ',');
+        std::getline(ss, creditStr, ',');
+        std::getline(ss, instructorName, ',');
+        std::getline(ss, instructorEmail, ',');
+        std::getline(ss, prereqStr, ',');
+
+        Instructor inst{instructorName, instructorEmail};
+        CourseDescription desc{title, "", std::stoi(creditStr), inst};
+        Course course(id, desc);
+
+        std::stringstream prereqStream(prereqStr);
+        std::string prereqID;
+        while (std::getline(prereqStream, prereqID, '|')) {
+            Course prereq(prereqID, {});
+            course.addPrerequisite(prereq);
+        }
+
+        courses[id] = course;
+    }
+
+    file.close();
+}
+
+
+void System_Manager::writeCoursesToFile() {
+    std::ofstream file("courses.csv");
+    if (!file.is_open()) {
+        std::cerr << " Failed to open courses.csv for writing\n";
+        return;
+    }
+
+    for (const auto& [id, course] : courses) {
+        file << id << ","
+             << course.getTitle() << ","
+             << course.getCreditHour() << ","
+             << course.getInstructorName() << ","
+             << course.getInstructorEmail() << ",";
+
+        const auto& prereqs = course.getPrerequisites();
+        for (size_t i = 0; i < prereqs.size(); ++i) {
+            file << prereqs[i].getCourseID();
+            if (i != prereqs.size() - 1)
+                file << "|";
+        }
+
+        file << "\n";
+    }
+
+    file.close();
+}
